25baaaff2f61b8bd3af7248414ef8d9a4816fb9a
node11 <124257857+node011@users.noreply.github.com>
2023-06-06 17:33:59 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,12 @@
 
 NGROK_HOME=/usr/local/ngrok
 
-#curl https://gitcode.net/cert/cn-acme.sh/-/raw/master/install.sh?inline=false | sh -s email=544218160@qq.com
-#
-#cd /usr/local
-#curl -o /usr/local/ngrok.tar.gz "https://gitee.com/lliubowen_94/docker-ngrok-server/raw/master/files/ngrok.tar.gz"
-#tar -zxvf /usr/local/ngrok.tar.gz -C /usr/local
-#rm -rf /usr/local/ngrok.tar.gz
-
-# 创建证书目录
-mkidr -p /usr/local/certs
-
 cd $NGROK_HOME
 
 # init ngrok server if build.info is not exist.
 if [ ! -f "build.info" ]; then
+
+  go version
   echo "init ngrok server!"
   DOMAIN=$1
   HTTP_PORT=$2
@@ -55,4 +47,4 @@ HTTP_PORT=$(sed -n "2p" build.info)
 HTTPS_PORT=$(sed -n "3p" build.info)
 TUNNEL_PORT=$(sed -n "4p" build.info)
 
-./bin/ngrokd -tlsKey=/usr/local/certs/$($DOMAIN).key -tlsCrt=/usr/local/certs/$($DOMAIN)_integrated.crt -domain="$DOMAIN" -httpAddr=":$HTTP_PORT" -httpsAddr=":$HTTPS_PORT" -tunnelAddr=":$TUNNEL_PORT"
+./bin/ngrokd -tlsKey=/usr/local/certs/device.key -tlsCrt=/usr/local/certs/device.crt -domain="$DOMAIN" -httpAddr=":$HTTP_PORT" -httpsAddr=":$HTTPS_PORT" -tunnelAddr=":$TUNNEL_PORT"
